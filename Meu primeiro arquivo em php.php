ff0bc78e1fa649b29c2927278cfeff1cfae38a53
Vjsantana <72237096+Vjsantana@users.noreply.github.com>
2021-01-08 16:24:48 -0300

    Comentários em PHP
    Inserção de comentários na linguagem.

--- Meu primeiro arquivo em php.php
+++ Meu primeiro arquivo em php.php
@@ -9,7 +9,7 @@
     <h1> Página Inicial em PHP</h1>
 
     <?php echo "<h2> Primeiros passos e detalhes da linguagem <h2/>"; ?>
-    
+
 
     <?php echo "Essas são as primeiras linhas de um código na linguagem PHP. Nesta linguagem, todas as varíáveis se iniciam com um '$'. Além disso, 
 é necessário ressaltar que na linguagem PHP há a diferenciação entre letras maiúsculas e minúsculas. Dessa forma, todas a variáveis serão escritas 
@@ -22,3 +22,18 @@ considerar o valor de uma variável como tal pois essas declarações de variáv
 ?>
 </body>
 </html>
+
+<?php "// Este é um tipo de comentário para uma única linha. Basta iniciar com '//';
+
+/* Este é um comentário de múltiplas linhas.
+Note que ele incia com (/*) e termina com (*/) */
+
+# Este é um comentário de uma linha no estilo shell. Note que ele inicia com #
+
+// OS EXEMPLOS DE COMENTÁRIOS APRESENTADOS ACIMA NÃO IRÃO SER LIDOS PELO COMPILADOR E CONSEQUENTEMENTE NÃO
+IRÃO APARECER NA PÁGINA WEB. NO ENTANTO, SE ELES FOREM INCLUÍDOS DENTRO DO 'BODY' DE UM CÓDIGO HTML, ELES
+APAREÇERÃO NA PÁGINA. O MESMO OCORRERÁ SE ELE VIER APÓS O COMANDO echo DO PHP.
+NOTE QUE ELES DEVEM FICAR ENTRE AS TAGS (<?php e ?>) DA LINGUAGEM PHP."
+?>
+
+
