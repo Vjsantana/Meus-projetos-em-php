ba5fbef71d48e8decc646d431e8e38182545040a
Vjsantana <72237096+Vjsantana@users.noreply.github.com>
2021-01-07 22:12:27 -0300

    Anotações em PHP
    Primeiros passos da lingugem.

---
+++ Meu primeiro arquivo em php.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Página de Teste</title>
+    <style>
+        h2 {
+            color: cadetblue;
+        }
+    </style>
+</head>
+<body>
+    <h1> Página Inicial em PHP</h1>
+
+    <?php echo "<h2> Primeiros passos e detalhes da linguagem <h2/>"; ?>
+
+<?php echo "Essas são as primeiras linhas de um código na linguagem PHP";
+
+?>
+</body>
+</html>
