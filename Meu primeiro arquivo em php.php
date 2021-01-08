6e6972663e6c5a43a29b320939a7c0cd9b36d36e
Vjsantana <72237096+Vjsantana@users.noreply.github.com>
2021-01-08 15:31:54 -0300

    Update Meu primeiro arquivo em php.php

--- Meu primeiro arquivo em php.php
+++ Meu primeiro arquivo em php.php
@@ -3,20 +3,15 @@
 <head>
     <meta charset="UTF-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Página de Teste</title>
-    <style>
-        h2 {
-            color: cadetblue;
-            text-shadow: 1px 1px brown;
-        }
-    </style>
+    <title>Página de Teste - Valmir</title>
 </head>
 <body>
     <h1> Página Inicial em PHP</h1>
 
     <?php echo "<h2> Primeiros passos e detalhes da linguagem <h2/>"; ?>
+    
 
-<?php echo "Essas são as primeiras linhas de um código na linguagem PHP. Nesta linguagem, todas as varíáveis se iniciam com um '$'. Além disso, 
+    <?php echo "Essas são as primeiras linhas de um código na linguagem PHP. Nesta linguagem, todas as varíáveis se iniciam com um '$'. Além disso, 
 é necessário ressaltar que na linguagem PHP há a diferenciação entre letras maiúsculas e minúsculas. Dessa forma, todas a variáveis serão escritas 
 com letras minúsculas. <br>
 Outro detalhe interessante é que nesse tipo de linguagem, não existe declaração de varíável assim como na linguagem Java, por exemplo. (String,
